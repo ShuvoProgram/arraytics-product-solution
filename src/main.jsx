2452ddd054b8ebba1416944163c61afa446403ca
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-30 16:10:57 +0600

    �️ Rectified table data discrepancy. � Fixed issues in table data handling. �️ Resolved table data inconsistencies. � Patched up table data irregularities. � Rectified errors in table data display.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react';
+// import { ChakraProvider } from '@chakra-ui/react';
 import { Toaster } from 'react-hot-toast';
 
 
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
   <React.StrictMode>
-  <ChakraProvider>
+  {/* <ChakraProvider> */}
       <App />
     <Toaster
       position="top-center"
@@ -26,7 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       }}
       reverseOrder={false}
     />
-    </ChakraProvider>
+    {/* </ChakraProvider> */}
   </React.StrictMode>
 </QueryClientProvider>,
 )
